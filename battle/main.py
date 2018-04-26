658d3701b4410c03ba0ce35633714a7ed750deee
amrutaarote <aroteamruta@gmail.com>
2018-04-26 18:42:00 +0530

    added new file and use magic wrt cost

--- battle/main.py
+++ battle/main.py
@@ -1,15 +1,32 @@
 from classes.game import Person, bcolors
+from classes.magic import Spell
 
-magic = [{"name": "Fire", "cost":10, "dmg": 60},
-         {"name": "Thunder", "cost":12, "dmg": 80},
-         {"name": "Blizzard", "cost":10, "dmg": 60}]
+# Create Black Magic
+fire = Spell("Fire", 10, 100,"black")
+thunder = Spell("Thunder", 10, 100,"black")
+blizzard = Spell("Blizzard", 10, 100,"black")
+meteor = Spell("Meteor", 20, 200,"black")
+quake = Spell("Quake", 14, 140,"black")
 
+# Create White Magic
+cure = Spell("Cure", 12, 120, "White Magic")
+cura = Spell("Cura", 18, 200, "White Magic")
 
-player = Person(200, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 25, magic)
+# Instantiate People
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, cure,cura])
+enemy = Person(1200, 65, 45, 25, [])
 
 running = True
 i = 0
+
+
+# Old Code for Player Magic
+'''
+magic = [{"name": "Fire", "cost":10, "dmg": 60},
+         {"name": "Thunder", "cost":12, "dmg": 80},
+         {"name": "Blizzard", "cost":10, "dmg": 60}]
+'''
+
 
 print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
 
@@ -27,19 +44,24 @@ while running:
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input("choose magic:")) - 1
-        magic_dmg = player.generate_spell_damage(magic_choice)
-        spell = player.get_spell_name(magic_choice)
-        cost = player.get_spell_mp_cost(magic_choice)
+
+        # magic_dmg = player.generate_spell_damage(magic_choice)
+        # spell = player.get_spell_name(magic_choice)
+        # cost = player.get_spell_mp_cost(magic_choice)
+
+
+        spell = player.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
 
         current_mp = player.get_mp()
 
-        if cost > current_mp:
+        if spell.cost > current_mp:
             print(bcolors.FAIL + "\nNot enough MP\n" + bcolors.ENDC)
             continue
 
-        player.reduce_mp(cost)
+        player.reduce_mp(spell.cost)
         enemy.take_damage(magic_dmg)
-        print(bcolors.OKGREEN + "\n" + spell + " deals ", str(magic_dmg), " points of damage " + bcolors.ENDC)
+        print(bcolors.OKGREEN + "\n" + spell.name + " deals ", str(magic_dmg), " points of damage " + bcolors.ENDC)
 
     enemy_choice = 1
 
